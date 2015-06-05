f9bd7b887e56b3ba62398c2fc37dd7c17eea3cdc
TaM <ryoki2k@gmail.com>
2015-06-05 19:57:28 +0200

    Added http://

--- app/assets/javascripts/angular-app/models/conference.js
+++ app/assets/javascripts/angular-app/models/conference.js
@@ -2,7 +2,7 @@ angular
   .module('app')
   .factory('Conference', function($resource) {
 
-    var Conference = $resource('http://localhost:3000/api/v1/conferences/:id.json', {id: '@id'}, {
+    var Conference = $resource('http://cmnew.herokuapp.com/api/v1/conferences/:id.json', {id: '@id'}, {
       update: {
         method: 'PUT'
       }
