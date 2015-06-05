588ebf7363504cc8e16553310d89db79a18b59d6
TaM <ryoki2k@gmail.com>
2015-06-05 16:00:49 +0200

    removed https

--- app/assets/javascripts/angular-app/models/conference.js
+++ app/assets/javascripts/angular-app/models/conference.js
@@ -2,7 +2,7 @@ angular
   .module('app')
   .factory('Conference', function($resource) {
 
-    var Conference = $resource('https://cmnew.herokuapp.com/api/v1/conferences/:id.json', {id: '@id'}, {
+    var Conference = $resource('cmnew.herokuapp.com/api/v1/conferences/:id.json', {id: '@id'}, {
       update: {
         method: 'PUT'
       }
